54810dc94eb03c879087835f935c7ca49349b180
Goofy9506 <goofy9605@protonmail.com>
2024-07-23 09:19:07 -0500

    chore: update variants and style

--- src/json/variants.ts
+++ src/json/variants.ts
@@ -1,5 +1,6 @@
 export const NewbShaders = [
-    { title: "Newb X Legacy", redirect: "/shaders?shader=newbxlegacy", img: "", desc: "By Devendrn" },
-    { title: "Newb X Future", redirect: "/shaders?shader=newbxfuture", img: "", desc: "By 1Sekon" },
-    { title: "NewbX Shaders", redirect: "/shaders/newbx", img: "", desc: "" },
+    { title: "Newb X Legacy", redirect: "/shaders?shader=newbxlegacy", img: "https://raw.githubusercontent.com/Goofy9506/render-dragon-shaders/main/src/images/newbx/legacy/logo.png", desc: "By Devendrn" },
+    { title: "Newb X Future", redirect: "/shaders?shader=newbxfuture", img: "https://raw.githubusercontent.com/Goofy9506/render-dragon-shaders/main/src/images/newbx/future/logo.png", desc: "By 1Sekon" },
+    { title: "Newb X Paretion", redirect: "/shaders?shader=newbxparetion", img: "https://raw.githubusercontent.com/Goofy9506/render-dragon-shaders/main/src/images/newbx/paretion/logo.png", desc: "By KuriMC" },
+    { title: "Newb X Trailer", redirect: "/shaders?shader=newbxtrailer", img: "https://raw.githubusercontent.com/Goofy9506/render-dragon-shaders/main/src/images/newbx/trailer/logo.png", desc: "By Ale" },
   ];
