b3709a09701ee2c580a0462df555db07b54daba0
Goofy9506 <goofy9605@protonmail.com>
2024-07-23 19:11:19 -0500

    add: newbx logo

--- src/json/shadersList.js
+++ src/json/shadersList.js
@@ -1,5 +1,5 @@
 const shadersList = [
-  { title: "Newb X Shaders", redirect: "./shaders.html?list=newbx", img: "", desc: "" },
+  { title: "Newb X Shaders", redirect: "./shaders.html?list=newbx", img: "./src/images/newbx/logo.png", desc: "" },
   { title: "NewbX Shaders", redirect: "/shaders/newbx", img: "", desc: "" },
   { title: "NewbX Shaders", redirect: "/shaders/newbx", img: "", desc: "" },
   { title: "NewbX Shaders", redirect: "/shaders/newbx", img: "", desc: "" },
